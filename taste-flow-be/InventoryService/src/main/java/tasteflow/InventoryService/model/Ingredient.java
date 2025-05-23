3a65373e72bbf00c3f5fae893732b272b2435a2f
Thu ha <nha3697@gmail.com>
2025-05-23 23:19:45 +0700

    Do: - Create project. - Done all models(4), validation.

---
+++ taste-flow-be/InventoryService/src/main/java/tasteflow/InventoryService/model/Ingredient.java
@@ -0,0 +1,32 @@
+package tasteflow.InventoryService.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Nationalized;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name="Ingredients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "Name can not blank !")
+    @Length(min = 10, message = "Name must greater than 10 chars")
+    @Nationalized
+    private String name;
+
+    @JoinColumn(name = "cateId")
+    @ManyToOne
+    private IngredientCategory category;
+
+    @Enumerated(EnumType.STRING)
+    private Unit unit;
+}
