e0c92e35674aa11a0d812a3cf97b2e994bccccb3
Shu1237 <ttei8191@gmail.com>
2025-05-23 13:59:23 +0700

    first commit fe

---
+++ taste-flow-fe/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-blue-500 text-white" variant={"outline"}>Login</Button>
+    </div>
+  );
+}
