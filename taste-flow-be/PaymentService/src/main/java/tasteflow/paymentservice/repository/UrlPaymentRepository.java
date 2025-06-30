45b5fcfb7ea39e766fe250f87c5d833edfd91912
HoangTuGio <khangdeptrai198@gmail.com>
2025-06-30 21:38:01 +0700

    Update Payment @Schedule to auto delete url payment out of date with isUsed:False

--- taste-flow-be/PaymentService/src/main/java/tasteflow/paymentservice/repository/UrlPaymentRepository.java
+++ taste-flow-be/PaymentService/src/main/java/tasteflow/paymentservice/repository/UrlPaymentRepository.java
@@ -3,6 +3,10 @@ package tasteflow.paymentservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tasteflow.paymentservice.model.UrlPayment;
 
+import java.time.LocalDateTime;
+
 public interface UrlPaymentRepository extends JpaRepository<UrlPayment, Integer> {
     UrlPayment findByorderId(int orderId);
+
+    void deleteByExpiryDateBeforeAndIsUsed(LocalDateTime expiryDate, boolean isUsed);
 }
--- taste-flow-be/PaymentService/src/main/java/tasteflow/paymentservice/service/UrlPaymentService.java
+++ taste-flow-be/PaymentService/src/main/java/tasteflow/paymentservice/service/UrlPaymentService.java
@@ -1,9 +1,13 @@
 package tasteflow.paymentservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tasteflow.paymentservice.model.UrlPayment;
 import tasteflow.paymentservice.repository.UrlPaymentRepository;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UrlPaymentService {
@@ -12,5 +16,13 @@ public class UrlPaymentService {
 
     public UrlPayment getUrlPayment(int orderId) {
         return urlPaymentRepository.findByorderId(orderId);
+    }
+
+    //Tự động xóa các url đã hết date mà chưa thanh toán
+    @Scheduled(fixedRate = 600000) // 10 phút = 600,000 milliseconds
+    @Transactional
+    public void cleanExpiredUrls() {
+        LocalDateTime now = LocalDateTime.now();
+        urlPaymentRepository.deleteByExpiryDateBeforeAndIsUsed(now,false);
     }
 }
