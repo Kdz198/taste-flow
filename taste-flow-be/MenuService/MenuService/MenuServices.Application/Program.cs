2dc77bf807e7b7778b443fbe9127e9501031f8f8
HoangTuGio <khangdeptrai198@gmail.com>
2025-07-07 21:30:32 +0700

    [Menu Services][Add] Configure Eureka client and update menu route paths

--- taste-flow-be/MenuService/MenuService/MenuServices.Application/Program.cs
+++ taste-flow-be/MenuService/MenuService/MenuServices.Application/Program.cs
@@ -4,6 +4,7 @@ using MenuServices.Application.Entities;
 using MenuServices.Application.Interfaces;
 using MenuServices.Application.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Steeltoe.Discovery.Client;
 
 namespace MenuServices.Application
 {
@@ -12,10 +13,12 @@ namespace MenuServices.Application
         public static void Main( string[] args )
         {
             var builder = WebApplication.CreateBuilder( args );
-
+            
             // Add services to the container.
-
             builder.Services.AddControllers();
+            
+            //Sign in with Eureka
+            builder.Services.AddDiscoveryClient(builder.Configuration);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -37,7 +40,9 @@ namespace MenuServices.Application
 
             app.UseAuthorization();
 
-
+            //Config Eureka Server
+            app.UseDiscoveryClient();
+            
             app.MapControllers();
 
             app.Run();
