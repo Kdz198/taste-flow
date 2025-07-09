1b80ecdb224e097394f1fdaf5221e43bcbbffd25
TieHung <hungptse183180@fpt.edu.vn>
2025-07-09 20:44:27 +0700

    [Menu Services] Update querry get all + test config local email git

---
+++ taste-flow-be/MenuService/MenuService/MenuServices.Application/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuServices.Application.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CategoriesController( IMediator mediator )
+        {
+            _mediator = mediator ?? throw new ArgumentNullException( nameof( mediator ) );
+        }
+
+
+    }
+}
