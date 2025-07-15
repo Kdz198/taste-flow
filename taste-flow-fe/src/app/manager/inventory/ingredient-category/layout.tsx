0503a980e25fde04ff22cf50ef0c97b9cb2562fd
Dev Wibu <tantnse184055@fpt.edu.vn>
2025-07-16 00:42:34 +0700

    feat: Implement Ingredient Categories management with modal and table components

---
+++ taste-flow-fe/src/app/manager/inventory/ingredient-category/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface ManagerIngredientCategoryLayoutProps {
+  children: React.ReactNode;
+}
+
+const ManagerIngredientCategoryLayout: React.FC<ManagerIngredientCategoryLayoutProps> = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default ManagerIngredientCategoryLayout;
