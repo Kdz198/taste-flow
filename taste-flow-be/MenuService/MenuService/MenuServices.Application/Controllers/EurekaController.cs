a7a01baeb02d4bcd5a58c431ad51ae3874418d50
HoangTuGio <khangdeptrai198@gmail.com>
2025-07-16 15:16:32 +0700

    feat: Add EurekaController for health and info endpoints

---
+++ taste-flow-be/MenuService/MenuService/MenuServices.Application/Controllers/EurekaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MenuServices.Application.Controllers
+{
+    [ApiController]
+    [Route("actuator")]
+    public class EurekaController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok(new { status = "UP" });
+        }
+
+        [HttpGet("info")]
+        public IActionResult Info()
+        {
+            return Ok(new
+            {
+                app = "MENU-SERVICE",
+                version = "1.0.0",
+                environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"
+            });
+        }
+    }
+}
